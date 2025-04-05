5952f6f737da44cad2abd773781b5050810e23b8
ashWhiteHat <phantomofrotten@gmail.com>
2025-04-05 14:45:12 +0900

    groth16 proof verification

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Poseidon Hash with Typescript
+
+A [poseidon hash](https://eprint.iacr.org/2019/458.pdf) implementation over BN254 and Circom circuit test.
+
+## Install
+
+```shell
+$ npm i poseidon-h
+```
+
+## Usage
+
+```ts
+import { poseidon } from "poseidon-h";
+
+const hash = poseidon([1n, 2n]);
+```
+
+## Groth16
+
+```shell
+$ yarn groth16
+```
--- tests/poseidon.test.ts
+++ tests/poseidon.test.ts
@@ -1,7 +1,37 @@
+import path from "path";
+const wasm = require("circom_tester").wasm;
+import { buildPoseidon } from "circomlibjs";
+import { groth16 } from "snarkjs";
 import { poseidon } from "../src";
 
 describe("poseidon", () => {
-  it("should work", () => {
-    expect(poseidon([1n, 2n])).toBeDefined();
+  const inputs = [1n, 2n];
+
+  it("should work with circomlibjs", async () => {
+    const circomPoseidon = await buildPoseidon();
+    const { F } = circomPoseidon;
+    const hash = circomPoseidon(inputs);
+    const hash2 = poseidon(inputs);
+
+    expect(F.toObject(hash)).toBe(hash2);
+  });
+
+  it("should work with circuit", async () => {
+    const circuit = await wasm(path.join("circuit", "poseidon.circom"));
+    const witness = await circuit.calculateWitness({ inputs });
+    const hash = poseidon(inputs);
+
+    await circuit.assertOut(witness, { out: hash });
+  });
+
+  it("should work with groth16", async () => {
+    const { proof, publicSignals } = await groth16.fullProve(
+      { inputs },
+      path.join("poseidon_js", "poseidon.wasm"),
+      path.join("setup_params", "poseidon_0001.zkey"),
+    );
+
+    expect(proof).toBeDefined();
+    expect(publicSignals).toBeDefined();
   });
 });
